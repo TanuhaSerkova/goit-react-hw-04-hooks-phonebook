d33ed186a57464d9134e489441386ebe45178a20
TanuhaSerkova <89786945+TanuhaSerkova@users.noreply.github.com>
2022-05-30 18:53:01 +0300

    pravki2

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,23 +5,19 @@ import { ContactForm } from 'components/ContactForm/ContactForm';
 import { ContactList } from 'components/ContactList/ContactList';
 import { Container, Section, TitleH1, TitleH2 } from './App.styled';
 
-export function App() {
-    const [contacts, setContacts] = useState([
-        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ]);
+const initialContacts = [
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
+
+export const App = () => {
+    const [contacts, setContacts] = useState(
+        () => JSON.parse(localStorage.getItem('contacts')) ?? initialContacts
+    );
     
     const [filter, setFilter] = useState('');
-
-    // проверка localStorage на наличие контактов
-    useEffect(() => {
-        const contactsStorage = localStorage.getItem('contacts');
-        const parcedContacts = JSON.parse(contactsStorage);
-
-        parcedContacts ? setContacts(parcedContacts) : setContacts([]);
-    }, []);
     
     // запись контакта в localStorage
     useEffect(() => {
